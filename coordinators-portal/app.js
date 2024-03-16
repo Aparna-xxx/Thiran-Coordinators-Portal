bfc5b864a6dbfb48a8b4e8cfe812e0b5987114e4
Mohan07 <mohanbcab@gmail.com>
2024-03-16 17:10:59 +0530

    updates

--- coordinators-portal/app.js
+++ coordinators-portal/app.js
@@ -4,21 +4,15 @@ const {db} = require('./db')
 const bodyParser = require('body-parser')
 const route = require('./routes/route')
 
-<<<<<<< HEAD
+
 const app = express();
 const PORT = 8000;
 
 const ejs = require("ejs");
 app.set('view engine', 'ejs');
 
-=======
-const ejs=require("ejs");
 
-
-const app = express();
-const PORT = 3000;
 app.set('view engine', 'ejs');
->>>>>>> b28a791eed937c6fa7ac2045a65caaf22a4fd38d
 db();
 
 const root = process.cwd();
