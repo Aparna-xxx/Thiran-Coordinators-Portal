ed6e2b79fe1dba5b40fa9d39c64c496843bae48a
Sudharsana000 <sudharsanaamuthan@gmail.com>
2024-03-17 16:41:15 +0530

    'ID'

--- coordinators-portal/routes/route.js
+++ coordinators-portal/routes/route.js
@@ -4,12 +4,14 @@ const Router = express.Router();
 const {login, home} = require('../controllers/loginController');
 const { attendance } = require('../controllers/attendanceController');
 const { winners } = require('../controllers/winnersControllers');
+const {updateattendance} = require('../controllers/attendanceController');
 
 
 Router.post('/',login)
       .get('/home', home)
       .get('/attendance',attendance)
       .get('/winner',winners)
+      .post('/updateAttendance',updateattendance)
 
 
 
