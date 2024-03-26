4e1fa167f6a644a98a601eb07a49dc3d26191a80
Aparna-xxx <aparnaramanathan14@gmail.com>
2024-03-26 16:08:35 +0530

    final continues

--- coordinators-portal/db.js
+++ coordinators-portal/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
+
+const mongourl = process.env.MONGOURL;
 
 const db =  async() => {
     try {
-        await mongoose.connect("mongodb+srv://aparnaramanthan14:1yKihI5MQ1N0qpAw@cluster0.0tmaiqa.mongodb.net/Thiran?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(mongourl)
         console.log("db connected");
 
     } catch (error) {
         console.error(error);
     }
 }
-
+//mongodb+srv://aparnaramanthan14:1yKihI5MQ1N0qpAw@cluster0.0tmaiqa.mongodb.net/Thiran?retryWrites=true&w=majority&appName=Cluster0
 module.exports = {db}
+
---
+++ coordinators-portal/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const {db} = require('./db')
+const bodyParser = require('body-parser')
+const route = require('./routes/route')
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT  || 5000;
+
+const ejs = require("ejs");
+app.set('view engine', 'ejs');
+
+
+app.set('view engine', 'ejs');
+db();
+
+const root = process.cwd();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/',route);
+
+
+app.use(express.static(path.join(root , 'public')))
+
+app.listen(PORT, () => {
+    console.log(`server is listening to port http://localhost:${PORT}`);
+});
+
+
+
